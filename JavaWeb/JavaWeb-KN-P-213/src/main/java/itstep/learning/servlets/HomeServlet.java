301e8eb2baecdd13dceefb62fa5e7f9f9fc0a85e
Vladyslav Karlinskyi <91985923+karlvava16@users.noreply.github.com>
2024-09-30 11:07:37 +0300

    30.09 11:07

---
+++ JavaWeb/JavaWeb-KN-P-213/src/main/java/itstep/learning/servlets/HomeServlet.java
@@ -0,0 +1,26 @@
+package itstep.learning.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected  void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+        //resp.getWriter().println("<h1>Home</h1>");
+    }
+
+}
+
+/*
+
+
+
+*/
+
